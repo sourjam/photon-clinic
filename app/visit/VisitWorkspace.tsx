b5fd6b8792b6fc4966ae6a469c4ef046c9332e9c
Jim Yang <jimyang@jims-mbp.mynetworksettings.com>
2026-08-25 13:20:13 -0400

    Build V2 foundation shell

---
+++ app/visit/VisitWorkspace.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+const bodyClasses = [
+  "flex",
+  "flex-col",
+  "gap-4",
+  "p-[14px_14px_18px]",
+  "flex-1",
+  "min-h-0",
+  "items-stretch",
+  "overflow-visible",
+  "wide:grid",
+  "wide:grid-cols-[minmax(0,62fr)_minmax(330px,38fr)]",
+  "wide:p-[16px_20px_20px]",
+  "wide:overflow-hidden",
+].join(" ");
+
+const columnClasses = [
+  "flex",
+  "flex-col",
+  "gap-3",
+  "min-w-0",
+  "wide:min-h-0",
+  "wide:overflow-y-auto",
+  "wide:overflow-x-hidden",
+  "wide:pr-1",
+].join(" ");
+
+export function VisitWorkspace() {
+  return (
+    <div className="flex h-screen flex-col bg-page">
+      <div data-region="prototype-chrome" />
+      <div className="flex min-h-0 flex-1 flex-col overflow-auto bg-page wide:overflow-hidden">
+        <header data-region="header" />
+        <div className={bodyClasses}>
+          <section className={columnClasses} data-region="left" />
+          <aside className={columnClasses} data-region="right" />
+        </div>
+        <div data-region="action-bar" />
+      </div>
+    </div>
+  );
+}
